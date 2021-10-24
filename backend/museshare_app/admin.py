b64e03a5e4dcd26bae1fed1254ae877b50159a0d
ARJUN SRIVASTAVA <arjunsrivastava01@gmail.com>
2021-10-24 00:43:00 -0700

    working on admin models

--- backend/museshare_app/admin.py
+++ backend/museshare_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MusicProject
+from .models import MusicProject, Genre
 
 admin.site.register(MusicProject)
+admin.site.register(Genre)
--- backend/museshare_app/models.py
+++ backend/museshare_app/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 # from custom.fields import SeparatedValuesField
 
-# Represents a single project
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=100, help_text='Enter a musical genre (e.g. Heavy Metal)')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
 class MusicProject(models.Model):
-    
+    """Model representing a single project"""    
     title = models.CharField(max_length=70, blank=False, default='', verbose_name='Title')
     description = models.CharField(max_length=200,blank=False, default='', verbose_name='Description')
     published = models.BooleanField(default=False, verbose_name='Project completed')
-    needs = models.CharField(max_length=70, blank=False, default='', verbose_name='Current needs')
     upload = models.FileField(upload_to='uploads/', verbose_name='Audio file')
-    tags = models.CharField(max_length=70, blank=False, verbose_name='Genre tags')
+    tags = models.CharField(max_length=70, blank=False, verbose_name='Identifier tags')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this song')
 
     # Metadata
     class Meta:
