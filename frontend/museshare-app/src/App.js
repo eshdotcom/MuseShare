6526a043ecabfdff5b0d411b05d3e0b1414972a3
Esh Sathiyamoorthy <eshwarsath@gmail.com>
2021-11-25 12:46:09 -0800

    implemented react routing for page navigation

--- frontend/museshare-app/src/App.js
+++ frontend/museshare-app/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
-import Navbar from './Components/Navbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './Pages/HomePage'
+import AboutUsPage from './Pages/AboutUsPage'
 import UploadForm from './Components/UploadForm'
-
+import CollaboratePage from './Pages/CollaboratePage'
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <UploadForm />
-    </div>
-  );
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about-us" component={AboutUsPage} />
+            <Route exact path="/upload" component={UploadForm} />
+            <Route exact path="/collaborate" component={CollaboratePage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
 }
 
 export default App;
