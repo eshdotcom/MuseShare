038a581d9fa3d1b253b7c20068208dcc30e270fc
Esh Sathiyamoorthy <eshwarsath@gmail.com>
2021-11-25 16:31:04 -0800

    changes in routing for UploadPage

--- frontend/museshare-app/src/App.js
+++ frontend/museshare-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage'
 import AboutUsPage from './Pages/AboutUsPage'
-import UploadForm from './Components/UploadForm'
+import UploadPage from './Pages/UploadPage'
 import CollaboratePage from './Pages/CollaboratePage'
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/about-us" component={AboutUsPage} />
-            <Route exact path="/upload" component={UploadForm} />
+            <Route exact path="/upload" component={UploadPage} />
             <Route exact path="/collaborate" component={CollaboratePage} />
           </Switch>
         </BrowserRouter>
